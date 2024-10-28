0476ab2eed03c93f49f6e14a5e45326c1ea57595
Brandon Arellano <arebrandon12@gmail.com>
2024-10-28 07:17:13 -0500

    Agregar archivos de proyecto.

---
+++ Arellano_Brandon_Exam1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Arellano_Brandon_Exam1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
