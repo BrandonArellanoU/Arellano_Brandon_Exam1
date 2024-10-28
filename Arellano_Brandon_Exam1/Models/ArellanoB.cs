603c5090c28fedfe53f2f241f1ed15631d7ab15f
Brandon Arellano <arebrandon12@gmail.com>
2024-10-28 07:40:02 -0500

    Creacion de la tabla con mi apellido y la de celular (modelo)

---
+++ Arellano_Brandon_Exam1/Models/ArellanoB.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Arellano_Brandon_Exam1.Models
+{
+    public class ArellanoB
+    {
+        [Required]
+        [MinLength(1)]
+        [MaxLength(150)]
+        public int edad { get; set; }
+        
+        public float altura { get; set; }
+        [MinLength(1)]
+        [Key]
+        [NotNull]
+        public string nombre { get; set; }
+        public bool esHombre { get; set; }
+
+        public DateTime fechaNacimiento { get; set; }
+
+        [ForeignKey(nameof(Celular))]
+        public Celular? celular { get; set; }
+
+
+
+    }
+}
---
+++ Arellano_Brandon_Exam1/Models/Celular.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Arellano_Brandon_Exam1.Models
+{
+    public class Celular
+    {
+        [Key]
+        [Required]
+        [MinLength(1)]
+        [MaxLength(200)]
+        [NotNull]
+        public string ID { get; set; }
+        [Required]
+        [MinLength(1)]
+        [MaxLength(200)]
+        [NotNull]
+        public string model { get; set; }
+        [Required]
+        [MinLength(1)]
+        [MaxLength(200)]
+        [NotNull]
+        public int year { get; set; }
+
+        [Required]
+        [MinLength(1)]
+        [MaxLength(200)]
+        [NotNull]
+        public float precio { get; set; }
+
+
+
+    }
+}
